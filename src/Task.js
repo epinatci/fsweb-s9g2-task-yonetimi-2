11a381e7482c4c38e2ec7cbf5ae204c194e31345
epinatci <128381420+epinatci@users.noreply.github.com>
2023-06-20 18:08:51 +0300

    tamamlandi

--- src/Task.js
+++ src/Task.js
@@ -1,11 +1,25 @@
 import React from "react";
+import { tr } from "date-fns/locale"
+import {differenceInDays,formatDistanceToNow} from "date-fns"
 
 const Task = ({ taskObj, onComplete }) => {
+
+  const dist = formatDistanceToNow(
+    new Date(taskObj.deadline),{
+    addSuffix: true,
+    locale: tr
+  }
+  );
+
+  const isDeadlineClose = differenceInDays(
+    new Date ( taskObj.deadline),
+    new Date ()
+  ) <=3 ;
 
   return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
+      <div className="deadline">son teslim: <span className={isDeadlineClose ? "bg-[#ffd9d4]" : "bg-[#d4d7ff]"}>{dist}</span></div>
       <p>{taskObj.description}</p>
       <div>
         {taskObj.people.map((p) => (
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+     "./src/**/*.{js,jsx,ts,tsx}"
+],
+  theme: {
+    extend: {
+      colors:{
+        pinar: "#ffd9d4"
+      }
+    },
+  },
+  plugins: [],
+}
+
